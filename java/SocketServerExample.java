12482b7a2abad81dcfad7eef9394057ac8905831
DESKTOP-U4TRVRI\Alumno 13 <dsantoyo@cocoschool.mx>
2022-09-27 15:01:28 -0500

    Terminado de trabajo server-cliente piedra, papel o tijera. :D

--- java/SocketServerExample.java
+++ java/SocketServerExample.java
@@ -6,6 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
+
 /**
 * This class implements java Socket server
 * @author pankaj
@@ -18,12 +19,19 @@ public class SocketServerExample {
 	private static ServerSocket server;
 	//socket server port on which it will listen
 	private static int port = 9876;
-
+	
 
 
 
 	public static void main(String args[]) throws IOException, ClassNotFoundException
 	{
+		int r,s,p;
+
+		r = 1;
+		s = 2;
+		p = 3;
+
+
 		
 		//create the socket server object
 		server = new ServerSocket(port);
@@ -35,16 +43,67 @@ public class SocketServerExample {
 			Socket socket = server.accept();
 			//read from socket to ObjectInputStream object
 			ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 			//convert ObjectInputStream object to String
 			String message = (String) ois.readObject();
-			System.out.println("Message Received: " + message);
-			//create ObjectOutputStream object
-			ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
-			//write object to Socket
-			oos.writeObject("Hi Client "+message);
-			//close resources
+			int numero = (int)(Math.random()*3+1);
+			System.out.println(numero);
+			int msg = Integer.parseInt(message);
+
+			if(msg == 1 && numero ==1)
+			{
+				oos.writeObject("roca vs roca \n empate");
+				System.out.println("escogiste roca ");
+
+			}
+			if(msg == 1 && numero ==2)
+			{
+				oos.writeObject("roca vs tijeras \nganaste");
+				System.out.println("escogiste tijeras ");
+			}
+			if(msg == 1 && numero ==3)
+			{
+				oos.writeObject("roca vs papel \nperdis");
+				System.out.println("escogiste papel ");
+			}
+			
+			// tijeras
+
+			if(msg == 2 && numero ==1)
+			{
+				oos.writeObject("tijeras vs roca \n perdiste");
+				System.out.println("escogiste roca ");
+			}
+			if(msg == 2 && numero ==2)
+			{
+				oos.writeObject("tijeras vs tijeras \n empete ");
+				System.out.println("escogiste tijeras ");
+			}
+			if(msg == 2 && numero ==3)
+			{
+				oos.writeObject("tijeras vs papel \n ganaste ");
+				System.out.println("escogiste papel ");
+			}
+
+			//papel
+
+			if(msg == 3 && numero ==1)
+			{
+				oos.writeObject("papel vs roca \n ganaste");
+				System.out.println("escogiste roca ");
+			}
+			if(msg == 3 && numero ==2)
+			{
+				oos.writeObject("papel vs tijeras \n perdiste");
+				System.out.println("escogiste tijeras ");
+			}
+			if(msg == 3 && numero ==3)
+			{
+				oos.writeObject("papel vs papel \n empate");
+				System.out.println("escogiste papel ");
+			}
+
 			ois.close();
-			oos.close();
 			socket.close();
 			//terminate the server if client sends exit request
 			if(message.equalsIgnoreCase("exit")) break;
