b4ca9b2bb7220376a56e9a48853f5b342c8bc41c
DESKTOP-U4TRVRI\Alumno 13 <dsantoyo@cocoschool.mx>
2022-09-27 15:08:01 -0500

    terminado parte de cliente, trabajo piedra,papel o tijera

--- java/SocketClientExample.java
+++ java/SocketClientExample.java
@@ -4,6 +4,7 @@ import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
 * This class implements java socket client
@@ -14,26 +15,62 @@ public class SocketClientExample {
 
 public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
 //get the localhost IP address, if server is running on some other IP, you need to use that
-InetAddress host = InetAddress.getLocalHost();
+//InetAddress host = InetAddress.getLocalHost();
+	InetAddress host = InetAddress.getByName("192.168.1.252");
 Socket socket = null;
+
+
+
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("ingresa tu opcion 1 = roca, 2 = tijeras, 3 = papel");
+
+    // String input
+    String name = myObj.nextLine();
+    int respuesta = Integer.parseInt(name);
+    boolean first = true;
+
+    // Numerical input
+   // int age = myObj.nextInt();
+ //   double salary = myObj.nextDouble();
+ if(respuesta == 1)
+ {
+         System.out.println("Elegiste roca");
+ }
+  if(respuesta == 2)
+ {
+         System.out.println("Elegiste tijeras");
+ }
+  if(respuesta == 3)
+ {
+         System.out.println("Elegiste papel");
+ }
+    // Output input by user
+   // System.out.println("Name: " + name);
+  //  System.out.println("Age: " + age);
+   // System.out.println("Salary: " + salary);
+
 ObjectOutputStream oos = null;
 ObjectInputStream ois = null;
-for(int i=0; i<5;i++){
+
+while(first == true){
 //establish socket connection to server
 socket = new Socket(host.getHostName(), 9876);
 //write to socket using ObjectOutputStream
 oos = new ObjectOutputStream(socket.getOutputStream());
 System.out.println("Sending request to Socket Server");
-if(i==4)oos.writeObject("exit");
-else oos.writeObject(""+i);
+
+oos.writeObject(name);
 //read the server response message
 ois = new ObjectInputStream(socket.getInputStream());
 String message = (String) ois.readObject();
 System.out.println("Message: " + message);
 //close resources
+first = false;
 ois.close();
 oos.close();
-Thread.sleep(100);
+
 }
 }
 }
+
